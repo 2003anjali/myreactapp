0f464c78f319686cbed0320c9727067c21f41f9c
2003anjali <anjali.nambiar2003@gmail.com>
2023-07-24 17:04:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from './Components/Home';
+import Courses from './Components/Courses';
+import About from './Components/About';
+import NavBar from './Components/NavBar';
 
+import {BrowserRouter ,Routes,Route} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     {/* <h1 className="bg-primary">React</h1>
+     <p>This is a paragraph</p> */}
+<BrowserRouter>
+<NavBar />
+<Routes>
+<Route path="/" element={<Home/>} />
+<Route path="/about" element={<About/>} />
+<Route path="/courses" element={<Courses/>} />
+</Routes>
+</BrowserRouter>
+   </>
   );
 }
 
